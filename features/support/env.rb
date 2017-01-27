db7152f0ceb0ce26ab84dc92e0ff4a3e77b41f0b
Ubuntu <ubuntu@ip-172-31-46-44.us-west-2.compute.internal>
2017-01-27 09:36:23 +0000

    Add comment about config file

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,6 +9,12 @@ Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, options)
 end
 
+# Load credentials from config.yml stored in the support directory.
+# Inside the file should look like:
+#
+# email: myemail@myemail.com
+# password: mypassword
+#
 config = YAML.load_file("#{File.dirname(__FILE__)}/config.yml")
 $login_email = config['login_email']
 $login_password = config['login_password']
